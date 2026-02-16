2fb9ea6611ce27be41709af656894142b2de9166
Finn Scheller <schfin01@students.zhaw.ch>
2026-02-16 18:32:01 +0000

    feat: added shadcn/ui added vite dev server migrated from yarn to pnpm

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts,tsx,js,jsx}",
+    "./components/**/*.{ts,tsx}",
+    "./dev/**/*.{html,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
